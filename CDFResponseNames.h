453e273362d1b844f2de7a40a77064001387ba6a
Patrick West <pwest@opendap.org>
2005-08-31 14:30:22 +0000

    macro for request and response names specific to cdf-handler

---
+++ CDFResponseNames.h
@@ -0,0 +1,12 @@
+// CDFResponseNames.h
+
+// 2004 Copyright University Corporation for Atmospheric Research
+
+#ifndef E_CDFResponseNames_H
+#define E_CDFResponseNames_H 1
+
+#define CDF_NAME "cdf"
+
+#endif // E_CDFResponseNames_H
+
+// $Log: CDFResponseNames.h,v $
