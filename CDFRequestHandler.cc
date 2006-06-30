793de6a928cebd03724e364eb2f6adcfa6f47b08
Patrick West <pwest@opendap.org>
2006-06-30 20:15:20 +0000

    In the help response added begin and end tags called handler to separate the different hander help responses and added a tag called name within the handler tag to say which handler this help is for

--- CDFRequestHandler.cc
+++ CDFRequestHandler.cc
@@ -116,6 +116,8 @@ bool
 CDFRequestHandler::cdf_build_help( BESDataHandlerInterface &dhi )
 {
     BESInfo *info = dynamic_cast<BESInfo *>(dhi.response_handler->get_response_object());
+    info->begin_tag( "Handler" ) ;
+    info->add_tag( "name", PACKAGE_NAME ) ;
     string handles = (string)DAS_RESPONSE
                      + "," + DDS_RESPONSE
                      + "," + DATA_RESPONSE
@@ -123,6 +125,7 @@ CDFRequestHandler::cdf_build_help( BESDataHandlerInterface &dhi )
                      + "," + VERS_RESPONSE ;
     info->add_tag( "handles", handles ) ;
     info->add_tag( "version", PACKAGE_STRING ) ;
+    info->end_tag( "Handler" ) ;
     return true ;
 }
 
